f5a10891b8c5b1e7ca87b6b022af7e1616c69f7d
mixxamm <maximjanssensj5@gmail.com>
2020-11-16 18:42:33 +0100

    Maak obstakel snelheid variabel
    Co-authored-by: WouterVanNueten <s107229@ap.be>

--- Assets/Scripts/ObstakelScript.cs
+++ Assets/Scripts/ObstakelScript.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class ObstakelScript : MonoBehaviour
 {
     public float zSpeed = 10;
+    public bool randomSpeed = true;
     private static Vector3 initialPosition;
     private static Quaternion initialRotation;
+    private float initialZSpeed;
     // Start is called before the first frame update
     void Start()
     {
         initialPosition = transform.localPosition;
         initialRotation = transform.rotation;
+        initialZSpeed = zSpeed;
     }
 
     // Update is called once per frame
@@ -20,11 +23,15 @@ public class ObstakelScript : MonoBehaviour
         float newZPos = transform.localPosition.z - (Time.deltaTime * zSpeed);
         transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, newZPos);
 
-        if(transform.localPosition.y < 0 || transform.localPosition.z < -18)
+        if(transform.localPosition.y < 0 || transform.localPosition.z < -19)
         {
             //respawn
             transform.localPosition = initialPosition;
             transform.rotation = initialRotation;
+            if (randomSpeed)
+            {
+                zSpeed = Random.Range(0.5f, 2.0f) * initialZSpeed;
+            }
         }
     }
 }
